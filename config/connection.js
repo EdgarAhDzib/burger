100c396ac288fcf057b634c2ddbc6b0029db013b
Edgar Martin del Campo <ixmati@comcast.net>
2016-11-11 15:07:47 -0600

    Updated .env connection

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
-		password: '',
+		password: 'El3ctro-K@tie',
 		database: 'burgers_db'
 	})
 }
